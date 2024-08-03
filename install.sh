df83a21b4def1a285019eb2109ff109422914cd5
someone0x0 <someone0x00@proton.me>
2024-08-03 23:32:36 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 echo "[*]   Strating..... "
 
- sudo apt update
-sudo apt install snort tcpdump
-sudo echo "alert tcp any any -> any 22 (msg:"Incoming SSH traffic detected"; sid:1000002; rev:1;)" >> sudo /etc/snort/rules/local.rules
-sudo echo "alert icmp any any -> any any (msg:"Large incoming ICMP packet detected - potential DoS"; dsize:>1024; sid:1000003; rev:1;)">>sudo  /etc/snort/rules/local.rules
-sudo mv tcpsnort /bin/tcpsnort
+apt update
+apt install snort tcpdump
+echo "alert tcp any any -> any 22 (msg:"Incoming SSH traffic detected"; sid:1000002; rev:1;)" >> /etc/snort/rules/local.rules
+echo "alert icmp any any -> any any (msg:"Large incoming ICMP packet detected - potential DoS"; dsize:>1024; sid:1000003; rev:1;)">> /etc/snort/rules/local.rules
+mv tcpsnort /bin/tcpsnort
+cp /etc/snort/rules/local.rules ../
+rm /etc/snort/rules/*
+mv /etc/snort/local.rules /etc/snort/rules
 
 
 echo "End instalation"
