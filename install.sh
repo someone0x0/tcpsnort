712114362c2d5dc9753295f4e64e26ecc89bf635
someone0x0 <someone0x00@proton.me>
2024-08-04 10:20:33 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -9,7 +9,14 @@ apt update
 apt install snort tcpdump
 echo "alert tcp any any -> any 22 (msg:"Incoming SSH traffic detected"; sid:1000002; rev:1;)" >> sudo /etc/snort/rules/local.rules
 echo "alert icmp any any -> any any (msg:"Large incoming ICMP packet detected - potential DoS"; dsize:>1024; sid:1000003; rev:1;)">>sudo  /etc/snort/rules/local.rules
+chmod +x tcpsnort
 mv tcpsnort /bin/tcpsnort
-
+read -p "Any Problem (y/n)" option 
+if [ $option -eq 'y' ];then
 rm install.sh
+
 echo "End instalation"
+else 
+echo "Try Again"
+fi
+exit 1
