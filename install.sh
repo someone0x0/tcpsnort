4ef2096858a4762931ac0521e2468b36930aacc0
someone0x0 <someone0x00@proton.me>
2024-08-04 10:15:49 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
+if [ "$(id -u)" -ne "0" ]; then
+    echo "This script must be run as root. Exiting."
+    exit 1
+fi
 echo "[*]   Strating..... "
 
 apt update
 apt install snort tcpdump
-
+echo "alert tcp any any -> any 22 (msg:"Incoming SSH traffic detected"; sid:1000002; rev:1;)" >> sudo /etc/snort/rules/local.rules
+echo "alert icmp any any -> any any (msg:"Large incoming ICMP packet detected - potential DoS"; dsize:>1024; sid:1000003; rev:1;)">>sudo  /etc/snort/rules/local.rules
 mv tcpsnort /bin/tcpsnort
 
-rm /etc/snort/rules/*
-
-echo "alert tcp any any -> any 22 (msg:"Incoming SSH traffic detected"; sid:1000002; rev:1;)" >> /etc/snort/rules/local.rules
-echo "alert icmp any any -> any any (msg:"Large incoming ICMP packet detected - potential DoS"; dsize:>1024; sid:1000003; rev:1;)">> /etc/snort/rules/local.rules
-
-
+rm install.sh
 echo "End instalation"
